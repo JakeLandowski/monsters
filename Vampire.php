18653d2a0dd3d2f5c45e6e9f2369163b4b4fc146
JakeLandowski <jakelandowski@gmail.com>
2018-02-08 13:21:48 -0800

    Made vampire class and made constructor

---
+++ Vampire.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *  Vampire.php
+ *  Author: Jake Landowski
+ *  Date: 2-8-18
+ */
+
+/**
+ *  Vampire.php
+ *  Date: 2-8-18
+ * 
+ *  @author Jake Landowski
+ */
+class Vampire extends Monster
+{
+    private $_victims;
+
+    public function __construct($name, $victims)
+    {
+        $this->_victims = $victims;
+    }
+}
