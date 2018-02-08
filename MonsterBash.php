148f14e80c89549ab5cb7338fa5dc76c8a6323a9
rmarlowg <rmarlow@mail.greenriver.edu>
2018-02-08 16:54:24 -0500

    Changed things

--- MonsterBash.php
+++ MonsterBash.php
@@ -9,7 +9,10 @@ $monster->getName();
 $monster->attack();
 
 $monster2 = new Vampire("Sparkles");
+echo "$monster2->getVictims()";
 $monster2->attack();
+echo "$monster2->getVictims()";
 
 $monster3 = new Dragon("Charmander");
 $monster3->attack();
+
