af045bbec4b3e0256b1b6bee74179cbfcc6bb5a3
JakeLandowski <jakelandowski@gmail.com>
2018-02-08 13:59:44 -0800

    Fixed formatting text

--- MonsterBash.php
+++ MonsterBash.php
@@ -3,16 +3,32 @@
 include ('Vampire.php');
 include ('Dragon.php');
 
+echo '<pre>';
 
+echo 'Made made new Monster';
 $monster = new Monster();
-$monster->getName();
-$monster->attack();
 
+echo 'Monster\'s name: ' . $monster->getName();
+echo 'Monster\'s attacking: ' . $monster->attack();
+
+
+echo 'Made made new Vampire';
 $monster2 = new Vampire("Sparkles");
-echo "$monster2->getVictims()";
-$monster2->attack();
-echo "$monster2->getVictims()";
 
+echo 'Vampire\'s name: ' . $monster2->getName() . '<br />';
+echo 'Vampire\'s attacking: ' . $monster2->attack() . '<br />';
+echo 'Vampire\'s victims before: ' . $monster2->getVictims() . '<br />';
+echo 'Vampire\'s attacking again: ' . $monster2->attack() . '<br />';
+echo 'Vampire\'s victims after: ' . $monster2->getVictims() . '<br />';
+
+echo 'Made made new Monster';
 $monster3 = new Dragon("Charmander");
-$monster3->attack();
 
+echo 'Dragon\'s name: ' . $monster3->getName() . '<br />';
+echo 'Dragon\'s attacking: ' . $monster3->attack() . '<br />';
+echo 'Dragon\'s victims before: ' . $monster3->getVictims() . '<br />';
+echo 'Dragon\'s attacking again: ' . $monster3->attack() . '<br />';
+echo 'Dragon\'s victims after: ' . $monster3->getVictims() . '<br />';
+
+
+echo '</pre>';
