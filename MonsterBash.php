51cc99b775e097d9c04bdbf84f5e02d7726a0d6b
rmarlowg <rmarlow@mail.greenriver.edu>
2018-02-08 16:38:46 -0500

    Added monster bash test class

---
+++ MonsterBash.php
@@ -0,0 +1,13 @@
+<?php
+
+monster = new Monster();
+
+monster.setName("Steve");
+monster.getName();
+monster.attack();
+
+monster2 = new Vampire("Sparkles");
+monster2.attack();
+
+monster3 = new Dragon("Charmander");
+monster3.attack();
